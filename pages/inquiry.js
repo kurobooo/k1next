315083289f048744e03a43e8bfa3f414ab77ceef
kurobooo <h.kuroda1@gmail.com>
2021-07-26 11:43:57 +0900

    問い合わせフォームのJS読み込みを追記

--- pages/inquiry.js
+++ pages/inquiry.js
@@ -9,9 +9,11 @@ export default function Inquiry() {
       title="お問い合わせ：K1ファクトリー 浜松市西区雄踏町でホームページ制作・チラシ・ポスターデータ制作ほか"
       description="中小規模のホームページ制作・管理、チラシ・ポスターデータ制作などを行います。ネット印刷を利用し製作価格を抑えます。"
       headerUrl="img/header-inq.jpg"
-      headerH1="お問い合わせ　K1ファクトリー">
+      headerH1="お問い合わせ　K1ファクトリー"
+      script="https://sdk.form.run/js/v2/embed.js">
       <div className="mx-auto px-6 py-6 text-center">
         <section>
+          <script src="https://sdk.form.run/js/v2/embed.js"></script>
           <div className="formrun-embed" data-formrun-form="@info-1626920606" data-formrun-redirect="true">
           </div>
         </section>
