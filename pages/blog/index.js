49be8e82e8e1356741fd731e05875bb651a7f259
kurobooo <h.kuroda1@gmail.com>
2021-08-06 16:37:10 +0900

    blog一覧のそれぞれのメニューごとにアニメーション追加

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,16 +1,42 @@
 import Layout from '../../components/layout'
 import Image from 'next/image'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
+
+const list = {
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.2,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    transition: {
+      when: "afterChildren",
+    },
+  },
+}
+
+const item = {
+  visible: { opacity: 1, y: 0 },
+  hidden: { opacity: 0, y: 100 },
+}
 
 export default function Blog({ blog }) {
   return (
     <Layout title="ブログ">
       <section className="pt-8">
         <h1 className="font-bold text-xl sm:text-2xl xl:text-4xl pt-20 pb-5 text-center">最近の投稿</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-5">
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={list}
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-5">
 
           {blog.map((blog) => (
-            <article className="col-span-1 mx-5 pb-5 cursor-pointer hover:opacity-75" key={blog.id}>
+            <motion.article variants={item} className="col-span-1 mx-5 pb-5 cursor-pointer hover:opacity-75" key={blog.id}>
               <Link href={`/blog/${blog.id}`}>
                 <figure>
                   <a><Image src={blog.eyecatch.url} alt={blog.title} width={1600} height={600} /></a>
@@ -19,10 +45,10 @@ export default function Blog({ blog }) {
               <Link href={`/blog/${blog.id}`}>
                 <h3><a>{blog.title}</a></h3>
               </Link>
-            </article>
+            </motion.article>
           ))}
 
-        </div>
+        </motion.div>
 
       </section>
     </Layout>
