6fb70db90b6cb37c132d405bcce0d322d65f8b84
kurobooo <h.kuroda1@gmail.com>
2021-08-17 23:55:20 +0900

    GAを再度process.env設定

--- pages/_document.js
+++ pages/_document.js
@@ -13,8 +13,11 @@ class MyDocument extends Document {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}');
-        `}}
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+        `,
+            }}
           />
         </Head>
         <body>
