1946d1f8512a910619bdd37727445aee7e45ddd1
kurobooo <h.kuroda1@gmail.com>
2021-09-14 23:08:46 +0900

    google analytics読み込みを最後に

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          {/* <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -18,7 +18,7 @@ class MyDocument extends Document {
             });
         `,
             }}
-          />
+          /> */}
         </Head>
         <body>
           <Main />
